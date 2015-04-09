20d2f6c570ac8067bd0d24ffe93b76350920d62c
Luke <toad1359@users.noreply.github.com>
2015-04-09 17:56:11 -0500

    Rough version of the language for writing modules.
    For the AI to write modules, a module should be written by a human, for it.

--- AILang.java
+++ AILang.java
@@ -1,7 +1,24 @@
-import 
+package AI;
 
-public class AILang {
-    public static void Modules() {
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AILang throws Exception {
     
+    String files[];
+    
+    public static void Modules() {
+        String workingDir = System.getProperty("user.dir");
+        File folder = new File(workingDir + "/modules/");
+        File[] listOfFiles = folder.listFiles();
+        files = new String[listOfFiles.length] ;
+        int count = 0;
+        for(File junk : listOfFiles) {
+            files[count] = junk;
+            count++;
+         }
     }
+    
 }
