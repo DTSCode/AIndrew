0d4d9e414bc82406de87e9a86035175fbcf01814
Luke <toad1359@users.noreply.github.com>
2015-04-11 14:46:32 -0500

    Update AI.java

--- AI.java
+++ AI.java
@@ -95,7 +95,7 @@ public class AI implements Runnable {
         	}
         }
         if (numToRespond >= 0) {
-            for (int i=0; i < numToRespond; numToRespond--) {
+            for (int i=-1; i < numToRespond; numToRespond--) {
                 parse(toRespond[i]);
             }
         }
