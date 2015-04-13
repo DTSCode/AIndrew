51a240f5c0f7770592c1934217390b4173f338a8
Luke <toad1359@users.noreply.github.com>
2015-04-12 21:29:27 -0500

    Update AI.java

--- AI.java
+++ AI.java
@@ -91,11 +91,11 @@ public class AI implements Runnable {
             }
             System.out.println("Broken up... getting chars");
             chars += getCharsInStringArr(fromBeginningPart);
-            System.out.println("Got quantity:" + chars);
-            if (line.split().length > 1) {
+            System.out.println("Got quantity: " + chars);
+            if (line.split(" ").length > 1) {
                 line = line.replace(line.substring(0, chars + (line.split().length - 1)), "");
             }
-            if (line.split().length <= 1) {
+            if (line.split(" ").length <= 1) {
             	line = line.replace(line.substring(0, chars), "");
             }
             for (int i = 0; i < fromBeginningPart.length; i++) {
